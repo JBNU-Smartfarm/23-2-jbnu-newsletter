8515c5f949da4d09b3c2c4007470d7c3a4195c88
dayoung <kallzero1008@jbnu.ac.kr>
2025-02-03 19:53:00 +0900

    upload project source code

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# 23-2-jbnu-newsletter
-2023년도 2학기 스마트농업프로그래밍 모니터링서비스(전북대 공지사항 뉴스레터)
+# JBNU-NoticeLetter
+- 2023년도 2학기 스마트농업프로그래밍(모니터링 서비스)
+- 개요: 전북대학교 웹사이트 공지사항에서 원하는 키워드가 포함된 글이 올라올 때 메일로 관련 정보를 받아볼 수 있습니다. 작업은 평일 10 ~ 18시 2시간 간격으로 실행되며, 아래 매뉴얼은 `@jbnu.ac.kr`을 사용하는 경우입니다.
+
+## How To Use  
+### 0. Fork
+### 1. GMail: ~@jbnu.ac.kr
+
+[보안 수준이 낮은 앱의 액세스](https://myaccount.google.com/lesssecureapps?pli=1&rapt=AEjHL4PZOeH6jzDHnTrdcpZ50qdFHgN6WEJmb5muJvWQP3DuLHQx5-M0abBYO6Jy1kx119Iu_cjOYxHbYej7So53JyXUaw29CQ) &rarr; 앱 허용: 사용으로 설정합니다.
+
+<img src='./asset/google.png' width=500/>
+
+### 2. Repository
+- Settings > (Security) Secrets and variables > Actions > ```New repository secret```
+- Name이 'MAIL_ADDRESS', 'MAIL_PASSWORD'인 변수 생성
+  - MAIL_ADDRESS(변수 이름): 메일 주소(변수 값)
+  - MAIL_PASSWORD(변수 이름): 메일 비밀번호(변수 값)
+  
+![](./asset/secret.png)
+
+- Settings > (Code and automation) Actions > General > Workflow permissions > ```Read and write permissions```
+
+![](./asset/permission.png)
+
+
+### 3. Custom Keywords
+- crawler.py: line 11에 원하는 키워드 입력 (예. ['벨트'], ['연수', '특강'] 등)
+
+  ```python
+  keywords = ['안내']
+  ```
+  
+### 4. Activate Actions
+Actions > ```I understand my workflows, go ahead and enable them``` > Python package > ```Enable workflow```
+![](./asset/actions.png)
+![](./asset/workflow.png)
+
+## Result
+![](./asset/result.png)
---
+++ crawler.py
@@ -0,0 +1,84 @@
+from bs4 import BeautifulSoup
+import urllib.request
+import smtplib
+from email.mime.text import MIMEText
+import os
+
+# KEYWORD ========================================
+# ================================================
+
+keywords = ['안내']
+
+# EMAIL ==========================================
+# ================================================
+
+email_address = os.environ.get('MAIL_ADDRESS')
+email_password = os.environ.get('MAIL_PASSWORD')
+
+# CODE ===========================================
+# ================================================
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(BASE_DIR, 'last_no.txt')
+
+if os.path.exists(file_path):
+    with open(file_path, 'r') as file:
+        last_no_file = file.read().split(',')
+        
+        if len(keywords) == len(last_no_file):
+            last_no_list = last_no_file
+        else:
+            last_no_list = [0 for i in range(len(keywords))]
+else:
+    last_no_list = [0 for i in range(len(keywords))]
+
+url = 'https://www.jbnu.ac.kr/web/news/notice/sub01.do?menu=2377'
+response = urllib.request.urlopen(url)
+html_content = response.read()
+
+soup = BeautifulSoup(html_content, 'html.parser')
+
+last_nums = []
+for idx, keyword in enumerate(keywords):
+    new_notices = []
+
+    last_no = int(list(last_no_list)[idx])
+
+    filtered_a_tags = [tag for tag in soup.find_all('a', class_='title') if keyword.lower() in tag.get_text().lower()]
+
+    for a_tag in filtered_a_tags:
+        # 고유 번호
+        no_index = int(a_tag.get('onclick', '').split('(')[-1].split(')')[0].strip("'"))
+        # 공지 제목
+        notice_title = a_tag.get_text(strip=True)
+        # 공지 세부내용 url
+        detail_url = f'https://www.jbnu.ac.kr/web/Board/{no_index}/detailView.do?pageIndex=1&menu=2377'
+
+        if last_no is None or no_index > last_no:
+            new_notices.append((no_index, notice_title, detail_url))
+
+    last_no = max(new_notices, key=lambda x: x[0])[0] if new_notices else last_no
+    last_nums.append(str(last_no))
+
+    if new_notices:
+        contents = '<ul>'
+        for notice in new_notices:
+            contents += f'<li>{notice[1]}<a href="{notice[2]}">(바로가기)</a></li>'
+        contents += '</ul>'
+
+        msg = MIMEText(f'키워드 "{keyword}"에 대한 새로운 공지사항이 업데이트되었습니다. 아래에서 상세 내용을 확인해주세요. \n \n{contents}', 'html')
+        msg['Subject'] = f'[{keyword}] 전북대학교 공지사항 새알림'
+        msg['From'] = email_address
+        msg['To'] = email_address
+
+        smtp = smtplib.SMTP('smtp.gmail.com', 587)
+        smtp.starttls()
+        smtp.login(email_address, email_password)
+        smtp.sendmail(email_address, email_address, msg.as_string())
+
+        smtp.quit()
+
+    new_notices.clear()
+
+with open(file_path, 'w') as file:
+    file.write(str(','.join(last_nums)))
